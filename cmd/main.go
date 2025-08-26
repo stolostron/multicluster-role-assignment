e099ed4454b5c72ada4a60aec98d501dcb7df6f2
Matthew Short <mshort@redhat.com>
2025-08-26 11:40:27 -0700

    Ran kubebuilder create api
    Signed-off-by: Matthew Short <mshort@redhat.com>

--- cmd/main.go
+++ cmd/main.go
@@ -36,6 +36,9 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/metrics/filters"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
+
+	rbacv1alpha1 "github.com/stolostron/multicluster-role-assignment/api/v1alpha1"
+	"github.com/stolostron/multicluster-role-assignment/internal/controller"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -47,6 +50,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
+	utilruntime.Must(rbacv1alpha1.AddToScheme(scheme))
 	// +kubebuilder:scaffold:scheme
 }
 
@@ -198,6 +202,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err := (&controller.MulticlusterRoleAssignmentReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "MulticlusterRoleAssignment")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if metricsCertWatcher != nil {
