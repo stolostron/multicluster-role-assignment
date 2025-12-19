2626b0ca6c849d096cfc4dde7ce1f1696f95c4a3
Matthew Short <mshort@redhat.com>
2025-12-19 00:07:13 +0000

    Removed unused prometheus kustomize related things, comments, and code
    Signed-off-by: Matthew Short <mshort@redhat.com>

--- cmd/main.go
+++ cmd/main.go
@@ -141,7 +141,6 @@ func main() {
 	// TODO(user): If you enable certManager, uncomment the following lines:
 	// - [METRICS-WITH-CERTS] at config/default/kustomization.yaml to generate and use certificates
 	// managed by cert-manager for the metrics server.
-	// - [PROMETHEUS-WITH-CERTS] at config/prometheus/kustomization.yaml for TLS certification.
 	if len(metricsCertPath) > 0 {
 		setupLog.Info("Initializing metrics certificate watcher using provided certificates",
 			"metrics-cert-path", metricsCertPath, "metrics-cert-name", metricsCertName, "metrics-cert-key", metricsCertKey)
